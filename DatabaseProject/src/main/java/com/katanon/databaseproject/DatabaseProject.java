d09e518dff451f35929f527385500420bd61bd7d
KTN-katanon <66160196@go.buu.ac.th>
2024-09-13 23:36:28 +0700

    Setup JDBC and Connect Database

--- DatabaseProject/src/main/java/com/katanon/databaseproject/DatabaseProject.java
+++ DatabaseProject/src/main/java/com/katanon/databaseproject/DatabaseProject.java
@@ -1,8 +1,11 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.katanon.databaseproject;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  *
@@ -11,6 +14,21 @@ package com.katanon.databaseproject;
 public class DatabaseProject {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Connection conn = null;
+        String url = "jdbc:sqlite:dcoffee.db";
+        try {
+            conn = DriverManager.getConnection(url);
+            System.out.println("Connection to SQLite has been establish");
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            if(conn!=null){
+                try {
+                    conn.close();
+                } catch (SQLException ex) {
+                    System.out.println(ex.getMessage());
+                }
+            }
+        }
     }
 }

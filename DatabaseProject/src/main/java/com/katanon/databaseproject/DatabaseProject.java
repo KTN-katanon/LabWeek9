53850a44618929756401f4f01aff1034879c8f69
KTN-katanon <66160196@go.buu.ac.th>
2024-09-13 23:35:25 +0700

    Create project

---
+++ DatabaseProject/src/main/java/com/katanon/databaseproject/DatabaseProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.katanon.databaseproject;
+
+/**
+ *
+ * @author nonku
+ */
+public class DatabaseProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

71951e06511b438832302bad6a10916cda37ed60
KTN-katanon <66160196@go.buu.ac.th>
2024-09-14 01:30:02 +0700

    Finish SelectDatabase

--- DatabaseProject/src/main/java/com/katanon/databaseproject/SelectDatabase.java
+++ DatabaseProject/src/main/java/com/katanon/databaseproject/SelectDatabase.java
@@ -4,10 +4,51 @@
  */
 package com.katanon.databaseproject;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author nonku
  */
 public class SelectDatabase {
-    
+    public static void main(String[] args) {
+        Connection conn = null;
+        String url = "jdbc:sqlite:dcoffee.db";
+        // Connection Database
+        try {
+            conn = DriverManager.getConnection(url);
+            System.out.println("Connection to SQLite has been establish");
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            return;
+        }
+        
+        //Selection
+        String sql = "SELECT * FROM category";
+        try {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+
+            while (rs.next()) {
+                System.out.println(rs.getInt("category_id") + " "
+                        + rs.getString("category_name"));
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        //Close Database
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+
+    }
 }

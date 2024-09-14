7c0062c69450334c3ddcb487d39628bee3e144fe
KTN-katanon <66160196@go.buu.ac.th>
2024-09-15 01:37:18 +0700

    change class name and delete comment

---
+++ DatabaseProject/src/main/java/com/katanon/ConectDatabase.java
@@ -0,0 +1,38 @@
+/*
+* Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+*/
+package com.katanon.databaseproject;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+*
+* @author nonku
+*/
+public class DatabaseProject {
+
+   public static void main(String[] args) {
+       Connection conn = null;
+       String url = "jdbc:sqlite:dcoffee.db";
+       try {
+           conn = DriverManager.getConnection(url);
+           System.out.println("Connection to SQLite has been establish");
+       } catch (SQLException ex) {
+           System.out.println(ex.getMessage());
+       } finally {
+           if(conn!=null){
+               try {
+                   conn.close();
+               } catch (SQLException ex) {
+                   System.out.println(ex.getMessage());
+               }
+           }
+       }
+}
+
+   }
+}

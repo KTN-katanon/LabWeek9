c24ef56df8e47db98b717775ca80bc3b83b9d9a3
KTN-katanon <66160196@go.buu.ac.th>
2024-09-16 18:24:54 +0700

    Update DatabaseHelper.java
    Create connection and close

--- DatabaseProject/src/main/java/com/katanon/databaseproject/helper/DatabaseHelper.java
+++ DatabaseProject/src/main/java/com/katanon/databaseproject/helper/DatabaseHelper.java
@@ -4,10 +4,42 @@
  */
 package com.katanon.databaseproject.helper;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author AVI003
  */
 public class DatabaseHelper {
+    private static Connection conn = null;
+    private static String url = "jdbc:sqlite:dcoffee.db";
+    static {
+        getConnect();
+    }
+    public static synchronized Connection getConnect(){
+        if (conn==null){
+            try {
+                conn = DriverManager.getConnection(url);
+                System.out.println("Connection to SQLite has been establish");
+            } catch (SQLException ex) {
+                Logger.getLogger(DatabaseHelper.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return conn;
+    }
     
+    public static synchronized void close(){
+        if(conn!=null){
+            try {
+                conn.close();
+                conn = null;
+            } catch (SQLException ex) {
+                Logger.getLogger(DatabaseHelper.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
 }

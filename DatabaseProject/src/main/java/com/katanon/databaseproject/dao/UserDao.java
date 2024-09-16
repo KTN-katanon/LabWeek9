8e83cbad699acdf6b87abe4e1949c613a1b8bb7d
KTN-katanon <66160196@go.buu.ac.th>
2024-09-16 18:25:18 +0700

    Update UserDao.java
    finish UserDao

--- DatabaseProject/src/main/java/com/katanon/databaseproject/dao/UserDao.java
+++ DatabaseProject/src/main/java/com/katanon/databaseproject/dao/UserDao.java
@@ -4,10 +4,37 @@
  */
 package com.katanon.databaseproject.dao;
 
+import com.katanon.databaseproject.model.User;
+
 /**
  *
  * @author AVI003
  */
-public class UserDao {
+public class UserDao implements Dao<User> {
+
+    @Override
+    public User get(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object getAll() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public User save(User obj) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public User update(User obj) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public int delete(User obj) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
